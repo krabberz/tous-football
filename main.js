442052bb8109b95ddf93832a9adec9cb6c2f8e37
krabbbbz <88599782+krabberz@users.noreply.github.com>
2026-09-21 01:23:16 +0000

    specifying countries

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Sample Data Fallbacks
 const COUNTRIES = [
   { id: 'england', name: 'England' },
   { id: 'france', name: 'France' },
@@ -7,31 +8,47 @@ const COUNTRIES = [
   { id: 'australia', name: 'Australia' }
 ]
 
-const ENGLAND_LEAGUES_STEP_1 = [
-  { id: 'nl', name: 'National League' }
-]
-
-const ENGLAND_LEAGUES_STEP_2 = [
-  { id: 'nl-n', name: 'National League North' },
-  { id: 'nl-s', name: 'National League South' }
-]
-
-const ENGLAND_LEAGUES_STEP_3 = [
-  { id: 'il-pd', name: 'Isthmian League Premier Division' },
-  { id: 'npl-pd', name: 'Northern Premier League Premier Division' },
-  { id: 'sl-pd-c', name: 'Southern League Premier Division Central' },
-  { id: 'sl-pd-s', name: 'Southern League Premier Division South' }
-]
+const LEAGUES_BY_COUNTRY = {
+  england: {
+    step1: [
+      { id: 'nl', name: 'National League' }
+    ],
+    step2: [
+      { id: 'nl-n', name: 'National League North' },
+      { id: 'nl-s', name: 'National League South' }
+    ],
+    step3: [
+      { id: 'il-pd', name: 'Isthmian League Premier Division' },
+      { id: 'npl-pd', name: 'Northern Premier League Premier Division' },
+      { id: 'sl-pd-c', name: 'Southern League Premier Division Central' },
+      { id: 'sl-pd-s', name: 'Southern League Premier Division South' }
+    ]
+  },
+  france: {
+    step1: [{ id: 'national', name: 'National' }],
+    step2: [{ id: 'national-2', name: 'National 2' }],
+    step3: [{ id: 'national-3', name: 'National 3' }]
+  },
+  germany: {
+    step1: [{ id: '3-liga', name: '3. Liga' }],
+    step2: [{ id: 'regionalliga', name: 'Regionalliga' }],
+    step3: [{ id: 'oberliga', name: 'Oberliga' }]
+  }
+}
 
 function route() {
   const hash = window.location.hash || '#countries'
 
+  // Hide all views
   document.querySelectorAll('.view').forEach(view => view.classList.add('hidden'))
 
   if (hash.startsWith('#leagues/')) {
     const countryId = hash.replace('#leagues/', '')
-    document.getElementById('selected-country-title').textContent = `${countryId} leagues`
-    renderLeagues()
+    const country = COUNTRIES.find(c => c.id === countryId)
+    const countryName = country ? country.name : countryId
+
+    document.getElementById('selected-country-title').textContent = `${countryName} Leagues`
+    renderLeagues(countryId)
     document.getElementById('view-leagues').classList.remove('hidden')
 
   } else if (hash.startsWith('#league/')) {
@@ -67,13 +84,22 @@ function renderCountries() {
   `).join('')
 }
 
-function renderLeagues() {
+function renderLeagues(countryId) {
   const step1 = document.getElementById('step-1-grid')
   const step2 = document.getElementById('step-2-grid')
   const step3 = document.getElementById('step-3-grid')
 
+  const countryData = LEAGUES_BY_COUNTRY[countryId]
+
+  if (!countryData) {
+    if (step1) step1.innerHTML = `<p style="grid-column: 1/-1; color: #888;">No leagues configured for this country yet.</p>`
+    if (step2) step2.innerHTML = ''
+    if (step3) step3.innerHTML = ''
+    return
+  }
+
   if (step1) {
-    step1.innerHTML = ENGLAND_LEAGUES_STEP_1.map(l => `
+    step1.innerHTML = (countryData.step1 || []).map(l => `
       <div class="league-card" onclick="window.location.hash='#league/${l.id}'">
         <div class="circle-icon"></div>
         <div>${l.name}</div>
@@ -82,7 +108,7 @@ function renderLeagues() {
   }
 
   if (step2) {
-    step2.innerHTML = ENGLAND_LEAGUES_STEP_2.map(l => `
+    step2.innerHTML = (countryData.step2 || []).map(l => `
       <div class="league-card" onclick="window.location.hash='#league/${l.id}'">
         <div class="circle-icon"></div>
         <div>${l.name}</div>
@@ -91,7 +117,7 @@ function renderLeagues() {
   }
 
   if (step3) {
-    step3.innerHTML = ENGLAND_LEAGUES_STEP_3.map(l => `
+    step3.innerHTML = (countryData.step3 || []).map(l => `
       <div class="league-card" onclick="window.location.hash='#league/${l.id}'">
         <div class="circle-icon"></div>
         <div>${l.name}</div>
